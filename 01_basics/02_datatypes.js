8dd4c02eaf398aa6d7b7a0420c9c1f65d78fe679
Siddharth Ghosh <87965978+Z-Maniac-Sidd@users.noreply.github.com>
2024-01-11 09:35:56 +0000

    L6

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";// treat all js as new versions of js
+// alert("hello") // we are using node js and not browser
+
+console.log(3+3) 
+console.log("hitseh")
+// code should be readable
+
+
+let name = "sidd"
+let age  = 18
+let isloggedin = false
+
+// number => 2^52 is range
+// bigint =>
+
+// string => double quote
+// boolean => true or false
+// null => standalone value
+//undefined => different from null, declare kar diya but kuch bhara nahi, null is representation of empty, like temp reading 0 nahi hai its null, abhi tak aaya nahi hai;
+// symbol =>unique elments ko identify karne help
+
+
+console.log(typeof "sidd")
+console.log(typeof null)
+console.log(typeof undefined)
